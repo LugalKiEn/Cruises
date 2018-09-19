e9356c92782f7dbd1d61dc12d6b3f52bbbb52228
LugalKiEn <ulasik1996@gmail.com>
2018-09-19 23:10:48 +0300

    Project completed
    Fully created website (witout any server).
    Added button 'To top'. Also added some animations for elements.

---
+++ js/script.js
@@ -0,0 +1,16 @@
+$(function() {
+  $('body').append('<button class = "btn-up" />');
+
+    $('.btn-up').click(function(){
+        $('body').animate({'scrollTop': 0}, 1000);
+        $('html').animate({'scrollTop': 0}, 1000);
+    });
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 200){
+            $('.btn-up').addClass('active');
+        }
+        else{
+            $('.btn-up').removeClass('active');
+        }
+    });
+});
